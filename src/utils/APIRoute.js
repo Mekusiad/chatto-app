a3c80ec363b0f6e1856ca6e3b869155264bffec7
Mauro Daisuke <mauro.daisuke@hotmail.com>
2024-03-22 10:58:55 -0400

    Adicionado variável de ambiente na URL

--- src/utils/APIRoute.js
+++ src/utils/APIRoute.js
@@ -1,10 +1,9 @@
-export const host = "http://localhost:3001";
-export const registerRoute = `${host}/register`;
-export const loginRoute = `${host}/login`;
-export const insertAvatarRoute = `${host}/insertAvatar`;
-export const addTask = `${host}/messages/insertAdd`;
-export const getAllMessages = `${host}/messages/getAllMessages`;
-export const getUserProfile = `${host}/getUserProfile`;
-export const getContacts = `${host}/getContacts`;
+export const registerRoute = `${process.env.REACT_APP_HOST}/register`;
+export const loginRoute = `${process.env.REACT_APP_HOST}/login`;
+export const insertAvatarRoute = `${process.env.REACT_APP_HOST}/insertAvatar`;
+export const addTask = `${process.env.REACT_APP_HOST}/messages/insertAdd`;
+export const getAllMessages = `${process.env.REACT_APP_HOST}/messages/getAllMessages`;
+export const getUserProfile = `${process.env.REACT_APP_HOST}/getUserProfile`;
+export const getContacts = `${process.env.REACT_APP_HOST}/getContacts`;
 
 export const urlAvatarApi = "https://api.multiavatar.com/";
