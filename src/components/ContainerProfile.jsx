471f58b72a31685b0dbcb1ba782ecf8a3e4c49d3
Mauro Daisuke <mauro.daisuke@hotmail.com>
2024-03-21 20:52:24 -0400

    Adicionado o componente ContainerProfile para melhor manutenção

---
+++ src/components/ContainerProfile.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import { BiPowerOff } from "react-icons/bi";
+import Button from "../components/Button.jsx";
+import Contact from "../components/Contact.jsx";
+
+import "../styles/ContainerProfile.css";
+
+const ContainerProfile = ({ userProfile, contacts, setSelectedChatUser }) => {
+  const [selectContact, setSelectContact] = useState(false);
+
+  const handleSelected = (index, userSelectTask) => {
+    setSelectContact(index);
+    setSelectedChatUser(userSelectTask);
+  };
+
+  return (
+    <div className="container-left">
+      <div className="container-profile">
+        <Button>
+          <BiPowerOff />
+        </Button>
+        <img src={userProfile.avatarImage} alt="profile-avatar" />
+        <p>{userProfile.username}</p>
+      </div>
+      <div className="contacts">
+        {contacts.map(
+          (user, index) =>
+            user.avatarImage && (
+              <Contact
+                key={index}
+                user={user}
+                index={index}
+                selectContact={selectContact}
+                handleSelected={handleSelected}
+              />
+            )
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default ContainerProfile;
